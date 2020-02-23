62731a1be72f863eb19330f2b481cf5810b62954
enricoirawan <enricoeusebius@gmail.com>
2020-02-23 21:00:35 +0700

    Updates

---
+++ precache-manifest.bae1e61fc8b6de9f85a4ff6d88ef5fa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ff90953fd03dee73110d635f2053b42",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c49383cd33a163452e3b",
+    "url": "/monster-rolodex/static/css/main.df5a20f7.chunk.css"
+  },
+  {
+    "revision": "01fa59518f48c0c9b0f6",
+    "url": "/monster-rolodex/static/js/2.650d6c94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.650d6c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c49383cd33a163452e3b",
+    "url": "/monster-rolodex/static/js/main.aaf85824.chunk.js"
+  },
+  {
+    "revision": "85cb2c00df0e1ad7d48d",
+    "url": "/monster-rolodex/static/js/runtime-main.3e508d1c.js"
+  }
+]);
